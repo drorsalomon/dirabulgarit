935352781cd6386405b8220b913ee3e12c7bca48
drorsalomon <drorsal3@gmail.com>
2024-06-08 17:37:10 +0300

    Added Zoho CRM key update

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -170,6 +170,59 @@ exports.getSiteMap = (req, res) => {
   res.status(200).render('siteMap');
 };
 
+const getZohoToken = async () => {
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: `https://accounts.zoho.com/oauth/v2/token?refresh_token=${process.env.ZOHO_REFRESH_TOKEN}&client_id=1000.UUNJFBGY0OU78GXWVYZO9G8G8KSJRI&client_secret=${process.env.ZOHO_CLIENT_SECRET}&grant_type=refresh_token`,
+    });
+
+    const zohoToken = res.data.access_token;
+    return zohoToken;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+
+const updateHerokuConfigVar = async (accessToken) => {
+  console.log(accessToken);
+  try {
+    const res = await axios({
+      method: 'PATCH',
+      url: `https://api.heroku.com/apps/${process.env.HEROKU_APP_NAME}/config-vars`,
+      data: {
+        ZOHO_ACCESS_TOKEN: accessToken,
+      },
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/vnd.heroku+json; version=3',
+        Authorization: `Bearer ${process.env.HEROKU_API_TOKEN}`,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+
+const getZohoRefreshToken = cron.schedule(
+  '*/3 * * * *',
+  async function () {
+    try {
+      const zohoToken = await getZohoToken();
+      await updateHerokuConfigVar(zohoToken);
+      console.log('***** Zoho Access Token Updated *****');
+    } catch (err) {
+      console.error(err);
+    }
+  },
+  {
+    scheduled: true,
+    timezone: 'Israel',
+  },
+);
+
 exports.getCalendlyLead = catchAsync(async (req, res) => {
   try {
     const calendlyLeadName = req.body.payload.name;
