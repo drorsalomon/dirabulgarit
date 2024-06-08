edecbd94777c7703add61972bb393f1f48d74d15
drorsalomon <drorsal3@gmail.com>
2024-06-08 23:37:32 +0300

    Calendly Payload testing

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -279,9 +279,11 @@ exports.getCalendlyLead = catchAsync(async (req, res) => {
       await axios({
         method: 'PUT',
         url: process.env.ZOHO_URL,
-        data: {
-          id: leadID,
-          Lead_Status: 'Canceled',
+        body: {
+          data: {
+            id: leadID,
+            Lead_Status: 'Canceled',
+          },
         },
         headers: {
           Authorization: `Zoho-oauthtoken ${process.env.ZOHO_ACCESS_TOKEN}`,
