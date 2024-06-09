83285bdb972adfb8101de12ab9321aae28d7e6d3
drorsalomon <drorsal3@gmail.com>
2024-06-09 18:45:01 +0300

    cron jobs debugging

--- services/cronJobs.js
+++ services/cronJobs.js
@@ -38,6 +38,7 @@ const getZohoRefreshToken = cron.schedule(
     try {
       let currentTimeDate = new Date();
       const zohoToken = await zohoService.getZohoToken();
+      console.log('Zoho Token:', zohoToken);
       await zohoService.updateHerokuConfigVar(zohoToken);
       console.log(`***** Zoho Access Token Updated at ${currentTimeDate.toLocaleString()} *****`);
     } catch (err) {
--- services/zohoService.js
+++ services/zohoService.js
@@ -20,6 +20,7 @@ exports.getZohoToken = catchAsync(async () => {
 });
 
 exports.updateHerokuConfigVar = catchAsync(async (accessToken) => {
+  console.log('Updating Heroku config with accessToken:', accessToken);
   try {
     await axios({
       method: 'PATCH',
